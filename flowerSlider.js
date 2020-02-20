5bb343f52e5165772b0c7cafa440f23eeebd5d36
aporia-coder <zachary_gray12@yahoo.com>
2020-02-20 13:17:15 +0000

    small changes

---
+++ flowerSlider.js
@@ -0,0 +1,7 @@
+const carouselSlider = document.querySelector(".carouselSlider");
+const carsouselImages = document.querySelectorAll(".carouselSlider img");
+
+//Counter
+let counter = 1;
+const size = carsouselImages[0].clientWidth;
+console.log(size);
